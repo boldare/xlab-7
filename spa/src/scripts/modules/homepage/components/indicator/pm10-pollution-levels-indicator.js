8321bc0dce601a651420b2e24d8850158f3b61d1
Krzysztof Sopa <krzysztof.sopa@xsolve.pl>
2017-01-20 09:46:14 +0100

    TASK-2

--- spa/src/scripts/modules/homepage/components/indicator/pm10-pollution-levels-indicator.js
+++ spa/src/scripts/modules/homepage/components/indicator/pm10-pollution-levels-indicator.js
@@ -1,34 +1,12 @@
 import React,{ PropTypes } from 'react';
 import { Row, Col } from 'react-materialize';
-import PollutionLevelsIndicator from './pollution-levels-indicator';
 
 class Pm10PollutionLevelsIndicator extends React.Component {
-  constructor() {
-    super();
-
-    this.levels = [
-      { lower: 0, upper: 20, class: 'black-text light-green', key: 'veryGood', label: 'Dobra' },
-      { lower: 21, upper: 60, class: 'black-text light-green lighten-2', key: 'good', label: 'Dobra' },
-      { lower: 61, upper: 100, class: 'black-text lime', key: 'moderate', label: 'Umiarkowana' },
-      { lower: 101, upper: 140, class: 'black-text amber lighten-1', key: 'sufficient', label: 'Dostateczna' },
-      { lower: 141, upper: 200, class: 'black-text grey', key: 'bad', label: 'Zła' },
-      { lower: 200, class: 'grey darken-4', key: 'veryBad', label: 'Bardzo zła' }
-    ];
-  }
-
   render() {
     return (
-      <PollutionLevelsIndicator
-        pollutionValue={this.props.pollutionValue}
-        levels={this.levels}
-        label='Poziom zanieczyszczenia pyłem PM 10'
-        indicator='pm_10' />
+      <b>Task-2: Write PM10 component</b>
     )
   }
-}
-
-Pm10PollutionLevelsIndicator.propTypes = {
-  pollutionValue: PropTypes.number
 }
 
 export default Pm10PollutionLevelsIndicator
--- spa/src/scripts/modules/homepage/components/indicator/pm25-pollution-levels-indicator.js
+++ spa/src/scripts/modules/homepage/components/indicator/pm25-pollution-levels-indicator.js
@@ -1,34 +1,12 @@
 import React,{ PropTypes } from 'react';
 import { Row, Col } from 'react-materialize';
-import PollutionLevelsIndicator from './pollution-levels-indicator';
 
 class Pm25PollutionLevelsIndicator extends React.Component {
-  constructor() {
-    super();
-
-    this.levels = [
-      { lower: 0, upper: 12, class: 'black-text light-green', key: 'veryGood', label: 'Bardzo dobra' },
-      { lower: 13, upper: 36, class: 'black-text light-green lighten-2', key: 'good', label: 'Dobra' },
-      { lower: 37, upper: 60, class: 'black-text lime', key: 'moderate', label: 'Umiarkowana' },
-      { lower: 61, upper: 84, class: 'black-text amber lighten-1', key: 'sufficient', label: 'Dostateczna' },
-      { lower: 85, upper: 120, class: 'black-text grey', key: 'bad', label: 'Zła' },
-      { lower: 120, class: 'grey darken-4', key: 'veryBad', label: 'Bardzo zła' }
-    ];
-  }
-
   render() {
     return (
-      <PollutionLevelsIndicator
-        pollutionValue={this.props.pollutionValue}
-        levels={this.levels}
-        label='Poziom zanieczyszczenia pyłem PM 2.5'
-        indicator='pm_25' />
+      <b>Task-2: Write PM2.5 component</b>
     )
   }
-}
-
-Pm25PollutionLevelsIndicator.propTypes = {
-  pollutionValue: PropTypes.number
 }
 
 export default Pm25PollutionLevelsIndicator
